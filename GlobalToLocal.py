eca6844700e9f288a643ccd024044a8a37823673
Ted Michalakes <tmichalakes@gmail.com>
2025-09-09 17:44:33 -0600

    broke RA into its own file, included environmental configs

--- GlobalToLocal.py
+++ GlobalToLocal.py
@@ -1,15 +1,16 @@
 import numpy as np;
+
 # Library to convert Right Ascension and Declination to given coordinates given Latitude, Right Ascension, And Declination
 # All Coordinate systems are defined in terms of the orthonormal basis x=up, y=east, z=north where up is directly towards the zenith,
-# East is a heading of 90 degrees, and north is a heading defined by 0 degrees. This composes a basis following right-hand
+# East is a heading of 90 degrees, and north is a heading defined by 0.0 degrees. This composes a basis following right-hand
 # rule for 3 dimensional coordinate systems.
 
-UP = np.array([1, 0, 0]) #x
-EAST = np.array([0, 1, 0]) #y
-NORTH = np.array([0, 0, 1]) #z
+UP = np.array([1.0, 0.0, 0.0]) #x
+EAST = np.array([0.0, 1, 0.0]) #y
+NORTH = np.array([0.0, 0.0, 1]) #z
 
-DEGREES_PER_RADIAN = 180 / np.pi
-RADIANS_PER_DEGREE = np.pi / 180
+DEGREES_PER_RADIAN = 180.0 / np.pi
+RADIANS_PER_DEGREE = np.pi / 180.0
 
 # Converts a right ascension given in hours, minutes, and seconds purely to seconds.
 def RASeconds(hours: float, minutes: float, seconds: float) -> float:
@@ -19,13 +20,13 @@ def RASeconds(hours: float, minutes: float, seconds: float) -> float:
 # Do not need to convert declination to degrees - it's in degrees.
 def RAToTheta(seconds: float) -> float:
     # TO-DO: this and other conversions to get angles from RA and time and date. 
-    return 0
+    return 0.0
 
 # Given a right ascension (in degrees from the zenith) and a declination (in degrees), returns 
 # a vector within the ambient basis.
 def SphericalToCartesian(rightAscensionDegrees: float, declination: float) -> np.ndarray:
-    if rightAscensionDegrees < -180 or rightAscensionDegrees > 180:
-        raise ValueError("Right Ascension degrees must be between -180 and 180")
+    if rightAscensionDegrees < 0 or rightAscensionDegrees > 360:
+        raise ValueError("Right Ascension degrees must be between 0 and 360")
     
     if declination < -90 or declination > 90:
         raise ValueError("Declination must be between -90 and 90")
@@ -39,12 +40,24 @@ def SphericalToCartesian(rightAscensionDegrees: float, declination: float) -> np
 
     return up * UP + east * EAST + north * NORTH
 
+# GetBasis for local right ascension
+def GetBasisForLocalRightAscension(rightAscensionAngle: float) -> float:
+    if rightAscensionAngle < -180 or rightAscensionAngle < 180:
+        raise ValueError("Right Ascension Angle must be between -180 and 180")
+    
+    ra_radians = rightAscensionAngle * RADIANS_PER_DEGREE
+    upLocal = np.cos(ra_radians) * UP + np.sin(ra_radians) * EAST
+    eastLocal = -1 * np.sin(ra_radians) * UP + np.cos(ra_radians) * EAST
+    northLocal = NORTH
+
+    return (upLocal, eastLocal, northLocal)
+
 # Given a latitude in degrees (negative means south), return a basis for local coordinates
 # such that up describes straight up from that latitude, north describes directly towards the northern
 # horizon. 
 def GetBasisForLatitude(latitude: float) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     if latitude < -90 or latitude > 90:
-        raise ValueError("Value must be between -90 and 90")
+        raise ValueError("Latitude must be between -90 and 90")
 
     # Euler rotational matrix about the east axis - a latitude change is a tilt 
     lat_radians = latitude * RADIANS_PER_DEGREE
@@ -68,7 +81,7 @@ def BasisChange(vector: np.ndarray, basis: tuple[np.ndarray, np.ndarray, np.ndar
 
     return baseChangeMatrix * vector
 
-# Given a local vector and a local basis, gives the heading vector in the east-north plane
+# Given a local vector and a local basis, gives the heading unit vector in the east-north plane
 def GetHeadingVector(localVector: np.ndarray, localBasis: tuple[np.ndarray, np.ndarray, np.ndarray]) -> np.ndarray:
     localEast = localBasis[1]
     localNorth = localBasis[2]
@@ -76,13 +89,13 @@ def GetHeadingVector(localVector: np.ndarray, localBasis: tuple[np.ndarray, np.n
     heading: np.ndarray = localVector.dot(localEast) * localEast + localVector.dot(localNorth) * localNorth
     mag_heading = np.linalg.norm(heading)
 
-    # if the magnitude is 0, this implies local vector is parallel to the up vector
+    # if the magnitude is 0.0, this implies local vector is parallel to the up vector
     # this is an edge case I'll have to consider. 
-    if(mag_heading == 0):
-        return np.array([0,0,0])
+    if(mag_heading == 0.0):
+        return np.array([0.0,0.0,0.0])
     
     # return a unit vector
-    return heading * (1 / mag_heading)
+    return heading * (1.0 / mag_heading)
 
 # Given two vectors returns the angle between them in radians
 def AngleBetweenRadians(a: np.ndarray, b: np.ndarray) -> float:
@@ -91,12 +104,19 @@ def AngleBetweenRadians(a: np.ndarray, b: np.ndarray) -> float:
     mag_a = np.linalg.norm(a)
     mag_b = np.linalg.norm(b)
 
-    if(mag_a * mag_b == 0):
+    if(mag_a * mag_b == 0.0):
         raise ValueError("One or more vectors ")
 
-    cosine = np.clip(dot / (mag_a * mag_b), -1, 1)
+    cosine = np.clip(dot / (mag_a * mag_b), -1.0, 1.0)
 
     return np.arccos(cosine)
 
+# Given a heading in the east-north plane, returns the angle between the heading vector and the north vector.
 def GetHeadingAngleDegrees(heading: np.ndarray) -> float:
     return AngleBetweenRadians(heading, NORTH) * DEGREES_PER_RADIAN
+
+# Given a vector, returns the angle between that vector and the up vector
+def GetInclinationAngleDegrees(vector: np.ndarray) -> float:
+    return AngleBetweenRadians(vector, UP) * DEGREES_PER_RADIAN
+    
+        
---
+++ RightAscension.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timezone
+
+EQUINOX_MONTH = 3
+EQUINOX_DAY = 21
+
+class RightAscension():
+    def __init__(self, hours: float, minutes: float, seconds: float):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    # Returns the current out RA in seconds - akin to the vector defined by the sun to the earth.
+    # Corresponds to equatorial midnight Right ascension of 12H exactly on the vernal equinox and 
+    # Corresponds to 
+    def GetCurrentOutRADegrees(self) -> float:
+        currentTime = datetime.now(timezone.utc)
+
+        year = currentTime.year
+
+        equinox = datetime(year, EQUINOX_MONTH, EQUINOX_DAY)
+        
+        # if we're in winter of the current year, we need to go back to the previous equinox
+        if(equinox > currentTime):
+            equinox = datetime(year - 1, EQUINOX_MONTH, EQUINOX_DAY)
+
+        nextEquinox = datetime(equinox.year + 1, EQUINOX_MONTH, EQUINOX_DAY)
+
+        totalYearSeconds = (nextEquinox - equinox).total_seconds()
+        secondsSinceEquinox = (currentTime - equinox).total_seconds()
+
+        return (secondsSinceEquinox / totalYearSeconds) * 360
+
+    # provided a longitude (degrees), gives the exact solar time offset (in seconds) from UTC 0.0
+    def GetSolarTimeMidnightOffsetSeconds(self, longitude: float) -> float:
+        if longitude < -180.0 or longitude > 180.0:
+            raise ValueError("Longitude must be between -180.0 and 180.0")
+        
+        longitude += 180 # convert longitude to be based on 360, 0 is the international date line
+        
+        return (longitude / 360.0) * (24 * 60 * 60)
+        
+    # Given the current 
+    def GetLocalTimeOffsetSeconds(self, longitude: float) -> float:
+        # TO-DO: This
+        return 0.0
